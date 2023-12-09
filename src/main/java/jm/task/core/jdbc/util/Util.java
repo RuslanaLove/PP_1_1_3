0167b6a425f60801957f8d0f3f4aebca6b8d4273
Vika101984 <vika.korobka3312@gmail.com>
2023-12-09 19:41:57 +0300

    In class Util add a configuration for Hibernate (without using xml), Service use the implementation dao through Hibernate, methods for creating and deleting a user table in the class UserHibernateDaoImpl implement using SQL

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,12 +16,33 @@ public class Util {
 
     private static Util instance;
 
+    private SessionFactory sessionFactory;
+
     private Util() {
         try {
             this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        try {
+            Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USERNAME);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 
     public static Util getInstance() {
